3c4ae500a8b8e7a5c571aca24ccafddbf654f1d7
mboy24399-pixel <mboy24399@gmail.com>
2026-09-01 10:31:21 +0530

    feat: add public privacy policy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,2 @@
+import LiveContentPage from '@/components/content/LiveContentPage';
+export default function PrivacyPolicyPage(){return <LiveContentPage contentKey="privacy" fallbackTitle="Privacy Policy"/>}
