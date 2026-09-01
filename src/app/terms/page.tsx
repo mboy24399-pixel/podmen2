49d680e17a75cc8c9fbacc2d24ddb597d7990dff
mboy24399-pixel <mboy24399@gmail.com>
2026-09-01 10:31:30 +0530

    feat: add public terms page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,2 @@
+import LiveContentPage from '@/components/content/LiveContentPage';
+export default function TermsPage(){return <LiveContentPage contentKey="terms" fallbackTitle="Terms & Conditions"/>}
