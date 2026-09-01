b5ef22e917237444c2c477487d064164c0316e41
mboy24399-pixel <mboy24399@gmail.com>
2026-09-01 17:02:33 +0530

    feat: add rewards page

---
+++ src/app/rewards/page.tsx
@@ -0,0 +1,3 @@
+"use client";
+import RewardsPanel from "@/components/rewards/RewardsPanel";
+export default function RewardsPage(){return <div className="mx-auto max-w-4xl p-5 pb-32 md:p-10"><RewardsPanel/></div>}
