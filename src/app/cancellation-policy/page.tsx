a8917013ae4031aeeb2c36fc554bd03b5869cd96
mboy24399-pixel <mboy24399@gmail.com>
2026-09-03 11:50:29 +0530

    Add public cancellation policy page

---
+++ src/app/cancellation-policy/page.tsx
@@ -0,0 +1,2 @@
+import LiveContentPage from '@/components/content/LiveContentPage';
+export default function CancellationPolicyPage(){return <LiveContentPage contentKey="cancellation" fallbackTitle="Cancellation Policy"/>}
