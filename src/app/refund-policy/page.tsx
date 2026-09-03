8b75a0fdaef8243d9813277c12546bfd868b38c9
mboy24399-pixel <mboy24399@gmail.com>
2026-09-03 11:50:20 +0530

    Add public refund policy page

---
+++ src/app/refund-policy/page.tsx
@@ -0,0 +1,2 @@
+import LiveContentPage from '@/components/content/LiveContentPage';
+export default function RefundPolicyPage(){return <LiveContentPage contentKey="refund" fallbackTitle="Refund Policy"/>}
