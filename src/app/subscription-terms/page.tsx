9486d12ab836cd33f2c3ad71edff1e7c3d4f3590
mboy24399-pixel <mboy24399@gmail.com>
2026-09-03 11:50:45 +0530

    Add public subscription billing page

---
+++ src/app/subscription-terms/page.tsx
@@ -0,0 +1,2 @@
+import LiveContentPage from '@/components/content/LiveContentPage';
+export default function SubscriptionTermsPage(){return <LiveContentPage contentKey="subscription" fallbackTitle="Subscription & Billing"/>}
